011cbef0d462b1e12b158df0dca50b73a1440090
Ernesto <egonzalez442@gmail.com>
2022-09-17 13:26:55 -0400

    Almost done, need image to work

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
-const contents = document.getElementById("contents");
-contents.parentNode.removeChild(contents);
+const contents = $("#contents");
+const contentsParent = contents.parent()
+contents.remove();
+$("contentsParent").prepend("./images/motivated")
