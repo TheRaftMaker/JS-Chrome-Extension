fca4d5275c3c6869a0ca5059a58d4b540a5a55aa
Ernesto <egonzalez442@gmail.com>
2022-09-18 19:23:46 -0400

    Everything is working, just need to style it a bit.

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
+const checkId = window.setInterval(main, 200);
+
+function main(){
 const contents = $("#contents");
+if(!contents[0]) return;
+
+window.clearInterval(checkId);
+
 const contentsParent = contents.parent();
 contents.remove();
-contentsParent.prepend("<img src='./images/motivated.jpg'>");
+
+contentsParent.prepend("<h1>Get back to Work!</h1>")
+// contentsParent.prepend("<img src='images/motivated.png'>");
+const imgURL = chrome.runtime.getURL("images/motivated.png");
+const htmlString = `<img src="${imgURL}">`;
+contentsParent.prepend(htmlString)
+}
